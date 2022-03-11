bd238f6d49a135e8d9aaaef13f8be18fb63d14f8
Megan Elise <megan.grim4@gmail.com>
2022-03-10 23:06:06 -0500

    worked on 10 but dont have names yet; not sure that i have it right

--- scripts.sql
+++ scripts.sql
@@ -461,4 +461,26 @@ ORDER BY nl_year;
 
 --Q 10 Find all players who hit their career highest number of home runs in 2016. Consider only players who have played in the league for at least 10 years, and who hit at least one home run in 2016. Report the players' first and last names and the number of home runs they hit in 2016.
 
-SELECT * from teams
+SELECT * from teams;
+
+SELECT * from battingpost;
+
+WITH a AS (SELECT playerid, 
+HR
+FROM battingpost
+ORDER BY HR desc),
+c AS (SELECT playerid, 
+HR
+FROM battingpost
+WHERE yearid = 2016
+AND HR >=1
+ORDER BY HR desc)
+
+SELECT playerid,
+a.HR as all_HR_ever,
+c.HR as HR_2016
+from a
+left join c
+USING (playerid)
+WHERE c.HR IS NOT NULL
+ORDER BY HR_2016 desc, all_HR_ever;
